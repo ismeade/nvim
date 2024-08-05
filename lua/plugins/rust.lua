3ef8c4cfba1f86768bd9fb7a50641a60c7f247e0
ismeade <ismeade99@sina.com>
2024-08-05 11:44:13 +0800

    up

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,40 @@
+return {
+    "simrat39/rust-tools.nvim",
+    dependencies = { -- optional packages
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+        require("rust-tools").setup({
+            tools = {
+                runnables = {
+                    use_telescope = true,
+                },
+                inlay_hints = {
+                    auto = true,
+                    show_parameter_hints = false,
+                    parameter_hints_prefix = "",
+                    other_hints_prefix = "",
+                },
+            },
+
+            server = {
+                -- on_attach is a callback called when the language server attachs to the buffer
+                on_attach = on_attach,
+                settings = {
+                    -- to enable rust-analyzer settings visit:
+                    -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+                    ["rust-analyzer"] = {
+                        -- enable clippy on save
+                        checkOnSave = {
+                            command = "clippy",
+                        },
+                    },
+                },
+            },
+
+        })
+
+    end
+}
